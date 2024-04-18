f5ac17f70a984e4073d9a23976115d77c76d731f
sebastian Whiffen <sebastianwhiffen@gmail.com>
2024-04-18 17:45:22 +1000

    change project layout

---
+++ src/RulesEngineWrapper.cs
@@ -0,0 +1,74 @@
+﻿using RulesEngine.Interfaces;
+using RulesEngine.Models;
+
+namespace RulesEngineWrapper;
+
+public class RulesEngineWrapper : IRulesEngine
+{
+    private readonly RulesEngine.RulesEngine _rulesEngine;
+
+    public RulesEngineWrapper(RulesEngine.RulesEngine rulesEngine)
+    {
+        _rulesEngine = rulesEngine;
+    }
+
+    public RulesEngineWrapper(string[] jsonConfig, ReSettings reSettings = null) : this(reSettings)
+    {
+        _rulesEngine = new RulesEngine.RulesEngine(jsonConfig, reSettings);
+    }
+
+    public RulesEngineWrapper(Workflow[] Workflows, ReSettings reSettings = null) : this(reSettings)
+    {
+         _rulesEngine = new RulesEngine.RulesEngine(Workflows, reSettings);
+    }
+
+    public RulesEngineWrapper(ReSettings reSettings = null)
+    {
+        _rulesEngine = new RulesEngine.RulesEngine(reSettings);
+    }
+   
+    public void AddOrUpdateWorkflow(params Workflow[] Workflows)
+    {
+        _rulesEngine.AddOrUpdateWorkflow(Workflows);
+    }
+
+    public void AddWorkflow(params Workflow[] Workflows)
+    {
+        _rulesEngine.AddWorkflow(Workflows);
+    }
+
+    public void ClearWorkflows()
+    {
+        _rulesEngine.ClearWorkflows();
+    }
+
+    public bool ContainsWorkflow(string workflowName)
+    {
+        return _rulesEngine.ContainsWorkflow(workflowName);
+    }
+
+    public ValueTask<ActionRuleResult> ExecuteActionWorkflowAsync(string workflowName, string ruleName, RuleParameter[] ruleParameters)
+    {
+        return _rulesEngine.ExecuteActionWorkflowAsync(workflowName, ruleName, ruleParameters);
+    }
+
+    public ValueTask<List<RuleResultTree>> ExecuteAllRulesAsync(string workflowName, params object[] inputs)
+    {
+        return _rulesEngine.ExecuteAllRulesAsync(workflowName, inputs);
+    }
+
+    public ValueTask<List<RuleResultTree>> ExecuteAllRulesAsync(string workflowName, params RuleParameter[] ruleParams)
+    {
+        return _rulesEngine.ExecuteAllRulesAsync(workflowName, ruleParams);
+    }
+
+    public List<string> GetAllRegisteredWorkflowNames()
+    {
+        return _rulesEngine.GetAllRegisteredWorkflowNames();
+    }
+
+    public void RemoveWorkflow(params string[] workflowNames)
+    {
+        _rulesEngine.RemoveWorkflow(workflowNames);
+    }
+}
