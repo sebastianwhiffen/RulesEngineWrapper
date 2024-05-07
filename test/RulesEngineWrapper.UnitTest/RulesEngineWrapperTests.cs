6d2212b0e8263b7f1dbd743119b3551a78f80713
sebastian Whiffen <sebastianwhiffen@gmail.com>
2024-05-07 11:21:45 +1000

    simplify test memberData

--- test/RulesEngineWrapper.UnitTest/RulesEngineWrapperTests.cs
+++ test/RulesEngineWrapper.UnitTest/RulesEngineWrapperTests.cs
@@ -10,43 +10,31 @@ public class RulesEngineWrapperTests
     {
         DatabaseFixture.EnsureInitializedAsync().GetAwaiter().GetResult();
     }
-    public static IEnumerable<object[]> ServiceCollections => DatabaseFixture._containers.Select(sc => new object[] { sc });
+    public static IEnumerable<object[]> rulesEngineWrappers => DatabaseFixture._containers.Select(container => new object[] { _factory.Create(container).BuildServiceProvider().GetRequiredService<IRulesEngineWrapper>()});
     public static RulesEngineWrapperFactory _factory = new RulesEngineWrapperFactory();
 
     [Theory]
-    [MemberData(nameof(ServiceCollections))]
-    public async Task AddWorkflow_ShouldWork(IContainer container)
+    [MemberData(nameof(rulesEngineWrappers))]
+    public async Task AddWorkflow_ShouldWork(IRulesEngineWrapper rulesEngineWrapper)
     {
-        var serviceProvider = _factory.Create(container).BuildServiceProvider();
-
-        var rulesEngineWrapper = serviceProvider.GetRequiredService<IRulesEngineWrapper>();
-
         var workflow = RulesEngineWrapperUtility.NewWorkflow();
 
         Assert.True(await rulesEngineWrapper.AddWorkflow(workflow), "Workflow should be added successfully");
     }
 
     [Theory]
-    [MemberData(nameof(ServiceCollections))]
-    public async Task AddOrUpdateWorkflow_ShouldWork(IContainer container)
+    [MemberData(nameof(rulesEngineWrappers))]
+    public async Task AddOrUpdateWorkflow_ShouldWork(IRulesEngineWrapper rulesEngineWrapper)
     {
-        var serviceProvider = _factory.Create(container).BuildServiceProvider();
-
-        var rulesEngineWrapper = serviceProvider.GetRequiredService<IRulesEngineWrapper>();
-
         var workflow = RulesEngineWrapperUtility.NewWorkflow();
 
         Assert.True(await rulesEngineWrapper.AddOrUpdateWorkflow(workflow), "Workflow should be added successfully");
     }
 
     [Theory]
-    [MemberData(nameof(ServiceCollections))]
-    public async Task RemoveWorkflow_ShouldWork(IContainer container)
+    [MemberData(nameof(rulesEngineWrappers))]
+    public async Task RemoveWorkflow_ShouldWork(IRulesEngineWrapper rulesEngineWrapper)
     {
-        var serviceProvider = _factory.Create(container).BuildServiceProvider();
-
-        var rulesEngineWrapper = serviceProvider.GetRequiredService<IRulesEngineWrapper>();
-
         var workflow = RulesEngineWrapperUtility.NewWorkflow();
 
         Assert.True(await rulesEngineWrapper.AddWorkflow(workflow), "Workflow should be added successfully");
